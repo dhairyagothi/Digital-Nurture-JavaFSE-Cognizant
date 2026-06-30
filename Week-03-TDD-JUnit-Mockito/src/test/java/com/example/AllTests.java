ce7449eba5c388891b0420abebbe0ad035477604
dhairyagothi <142989448+dhairyagothi@users.noreply.github.com>
2026-06-30 19:21:31 +0530

    week-3-day-3

--- Week-03-TDD-JUnit-Mockito/src/test/java/com/example/AllTests.java
+++ Week-03-TDD-JUnit-Mockito/src/test/java/com/example/AllTests.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import com.example.advanced.*;
 
 @Suite
 @SelectClasses({
@@ -11,7 +12,13 @@ import org.junit.platform.suite.api.Suite;
     EvenCheckerTest.class,
     OrderedTests.class,
     ExceptionThrowerTest.class,
-    PerformanceTesterTest.class
+    PerformanceTesterTest.class,
+    MyServiceTest.class,
+    ServiceTest.class,
+    ApiServiceTest.class,
+    FileServiceTest.class,
+    NetworkServiceTest.class,
+    MultiReturnServiceTest.class
 })
 public class AllTests {
 }
